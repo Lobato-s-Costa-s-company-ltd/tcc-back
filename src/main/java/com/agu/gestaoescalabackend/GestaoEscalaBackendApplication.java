2aa0e82bbfd8aa6a11c3fee172deddaada726af5
João Paulo Costa <joao-paulocosta@live.com>
2022-08-19 17:00:18 -0300

    added application log initialization

--- src/main/java/com/agu/gestaoescalabackend/GestaoEscalaBackendApplication.java
+++ src/main/java/com/agu/gestaoescalabackend/GestaoEscalaBackendApplication.java
@@ -1,13 +1,16 @@
 package com.agu.gestaoescalabackend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class GestaoEscalaBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoEscalaBackendApplication.class, args);
+		log.info("Application started");
 	}
 
 }

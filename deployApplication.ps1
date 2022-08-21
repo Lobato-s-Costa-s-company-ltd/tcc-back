b425555f4c1817b739514e3d6878c5f76a508d69
João Paulo Costa <59070553+oajoj@users.noreply.github.com>
2022-08-21 10:32:36 -0300

    Update deployApplication.ps1

--- deployApplication.ps1
+++ deployApplication.ps1
@@ -1,6 +1,9 @@
-Move-Item -Path 'C:\Users\joao-\Documents\Service Server Files\target' -Destination 'C:\Users\joao-\Documents\Service Server Files\backup' -Force
+$TargetFolder = 'C:\Users\joao-\Documents\Service Server Files\target'
+$BackupFolder = 'C:\Users\joao-\Documents\Service Server Files\backup'
 
-Remove-Item 'C:\Users\joao-\Documents\Service Server Files\target' -Force -Recurse
+if(Test-Path -Path $TargetFolder){
+    Move-Item -Path 'C:\Users\joao-\Documents\Service Server Files\target' -Destination 'C:\Users\joao-\Documents\Service Server Files\backup' -Force
+}
 
 Copy-Item -Path 'C:\Users\joao-\Documents\tcc\actions-runner\_work\tcc-back\tcc-back\target' -Destination 'C:\Users\joao-\Documents\Service Server Files' -Recurse -Force
 
@@ -12,8 +15,10 @@ $isServiceRunning = Get-Service MyApp | Where-Object {$_.Status -eq "Running"}
 
 if (-Not $isServiceRunning) {
     Remove-Item 'C:\Users\joao-\Documents\Service Server Files\target' -Force -Recurse
-    Move-Item -Path 'C:\Users\joao-\Documents\Service Server Files\backup' -Destination 'C:\Users\joao-\Documents\Service Server Files\target' -Force -Recurse
-    Remove-Item 'C:\Users\joao-\Documents\Service Server Files\backup' -Force -Recurse
+    if(Test-Path -Path $BackupFolder){
+        Move-Item -Path 'C:\Users\joao-\Documents\Service Server Files\backup\target' -Destination 'C:\Users\joao-\Documents\Service Server Files' -Force
+        Remove-Item 'C:\Users\joao-\Documents\Service Server Files\backup' -Force -Recurse
+    }
     Stop-Service MyApp
     Start-Service MyApp
     Write-Error 'There was a problem during application deployment. All changes were reverted and the server was restarted'
@@ -21,6 +26,8 @@ if (-Not $isServiceRunning) {
     throw "Application wasn't sucessfully initialized"
 }
 
-Remove-Item 'C:\Users\joao-\Documents\Service Server Files\backup' -Force -Recurse
+if(Test-Path -Path $BackupFolder){
+    Remove-Item 'C:\Users\joao-\Documents\Service Server Files\backup' -Force -Recurse
+}
 
 exit $LASTEXITCODE
